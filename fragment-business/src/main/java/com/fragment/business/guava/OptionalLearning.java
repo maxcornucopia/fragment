44a2a230cdc5e057f77a18496ef9e425648d7c75
leo <minispace2012@163.com>
2017-07-31 23:22:48 +0800

    guava optional learning

---
+++ fragment-business/src/main/java/com/fragment/business/guava/OptionalLearning.java
@@ -0,0 +1,35 @@
+package com.fragment.business.guava;
+
+import com.google.common.base.Optional;
+
+/**
+ * Created by leo on 2017/7/31.
+ */
+public class OptionalLearning {
+
+    public static void main(String[] args) {
+
+        //---------1.Optional<T>.of(T) 当T为Null直接抛NullPointException 2.T get() 获取Optional的值,当Optional的值为null时，抛出IllegalStateException-------------------------------
+//        //抛空指针异常
+//        Optional<Object> nullOf = Optional.of(null);
+//        System.out.println("----------------"+nullOf.get());
+
+        Optional<Integer> of = Optional.of(1);
+        System.out.println("----------------" + of.get());
+
+        //--------Optional<T> fromNullable(@Nullable T nullableReference) 当T为Null则使用默认值,可与or方法一起用------------------
+        Optional<Object> objectOptional = Optional.fromNullable(null);
+        System.out.println("----------------" + objectOptional);
+
+        //-----------------当Optional的值为null时，使用or赋予的值返回,可与fromNullable一起使用---------------
+        Object or = Optional.fromNullable(null).or(Integer.valueOf(1));
+        System.out.println("----------------" + or);
+
+        //-----------------isPresent() 如果Optional存在值，则返回True---------------
+        Optional<Object> nullOptional = Optional.fromNullable(null);
+        System.out.println("----------------" + nullOptional.isPresent());
+        System.out.println("----------------" + of.isPresent());
+
+
+    }
+}
